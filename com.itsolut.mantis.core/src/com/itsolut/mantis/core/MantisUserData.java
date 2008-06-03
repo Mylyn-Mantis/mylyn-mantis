9f1fd3431dfd9028fb2c72f9a92bc492cf121801
David Carver <d_a_carver@yahoo.com>
2008-06-03 06:42:47 +0000

    Mylyn 3.x Development

--- com.itsolut.mantis.core/src/com/itsolut/mantis/core/MantisUserData.java
+++
@@ -1,9 +0,0 @@
-package com.itsolut.mantis.core;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class MantisUserData {
-	Map<String, String[]> usersPerProject = new HashMap<String, String[]>();
-	
-}
