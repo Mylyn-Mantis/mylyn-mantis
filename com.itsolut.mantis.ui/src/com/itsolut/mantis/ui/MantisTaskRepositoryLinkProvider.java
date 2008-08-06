7e6d531a8aa6088051921e60a2c2a3622e78e71c
Robert Munteanu <rombert@sourceforge.net>
2008-08-06 07:06:57 +0000

    branched 2.x

---
+++ com.itsolut.mantis.ui/src/com/itsolut/mantis/ui/MantisTaskRepositoryLinkProvider.java
@@ -0,0 +1,48 @@
+package com.itsolut.mantis.ui;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ProjectScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.core.TaskRepositoryManager;
+import org.eclipse.mylyn.tasks.ui.AbstractTaskRepositoryLinkProvider;
+import org.eclipse.mylyn.tasks.ui.TasksUiPlugin;
+
+public class MantisTaskRepositoryLinkProvider extends
+		AbstractTaskRepositoryLinkProvider {
+
+	private static final String PROPERTY_PREFIX = "project.repository";
+
+	private static final String PROJECT_REPOSITORY_KIND = PROPERTY_PREFIX
+			+ ".kind";
+
+	private static final String PROJECT_REPOSITORY_URL = PROPERTY_PREFIX
+			+ ".url";
+
+	public TaskRepository getTaskRepository(IResource resource,
+			TaskRepositoryManager repositoryManager) {
+
+		IProject project = resource.getProject();
+		
+		if (project == null || !project.isAccessible())
+			return null;
+
+		IScopeContext projectScope = new ProjectScope(project);
+		IEclipsePreferences projectNode = projectScope
+				.getNode(TasksUiPlugin.ID_PLUGIN);
+		if (projectNode == null)
+			return null;
+
+		String kind = projectNode.get(PROJECT_REPOSITORY_KIND, "");
+		String urlString = projectNode.get(PROJECT_REPOSITORY_URL, "");
+
+		TaskRepository repository = repositoryManager.getRepository(kind,
+				urlString);
+
+		return repository;
+
+	}
+
+}
