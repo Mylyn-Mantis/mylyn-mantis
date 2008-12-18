708ac2036ff3dbe4ca7275b5147ad3a7ee5a5188
Robert Munteanu <rombert@sourceforge.net>
2008-12-18 07:22:15 +0000

    Tagged 3.0.1.200812171848

---
+++ com.itsolut.mantis.tests/src/com/itsolut/mantis/tests/AbstractMantisClientRepositoryTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package com.itsolut.mantis.tests;
+
+import com.itsolut.mantis.core.IMantisClient;
+import com.itsolut.mantis.core.MantisClientFactory;
+
+/**
+ * Test cases for classes that implement Mantis Client
+ * 
+ * @author Steffen Pingel
+ */
+public class AbstractMantisClientRepositoryTest extends AbstractMantisClientTest {
+
+	public AbstractMantisClientRepositoryTest(IMantisClient.Version version) {
+		super(version);
+	}
+
+	public void testValidateHTTP() throws Exception {
+		String username = "reporter";
+		String password = "reporter";
+			
+		IMantisClient mantisClient = MantisClientFactory.createClient(MantisTestConstants.TEST_MANTIS_HTTP_URL, IMantisClient.Version.MC_1_0a5, username, password, null, null, null);
+		mantisClient.validate();
+	}
+
+}
---
+++ com.itsolut.mantis.tests/src/com/itsolut/mantis/tests/MantisUtilsTest.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package com.itsolut.mantis.tests;
+
+import junit.framework.TestCase;
+
+import com.itsolut.mantis.core.MantisPriorityLevel;
+import com.itsolut.mantis.core.util.MantisUtils;
+
+/**
+ * @author Steffen Pingel
+ * @author David Carver
+ */
+public class MantisUtilsTest extends TestCase {
+
+	/**
+	 * Check for the values that indicate a task is completed.  This
+	 * is used by Mylyn to determine if a task is Completed and can be
+	 * filtered.
+	 * 
+	 * @author David Carver
+	 */
+	public void testIsCompleted() {
+		assertTrue(MantisUtils.isCompleted("closed"));
+		assertTrue(MantisUtils.isCompleted("resolved"));
+		assertFalse(MantisUtils.isCompleted("Closed"));
+		assertFalse(MantisUtils.isCompleted("Resolved"));
+	}
+
+	/**
+	 * Return the priorities as stored in Mantis.  Currently works with
+	 * the Priority Enums in the MantisTask class.
+	 * @author David Carver
+	 */
+	public void testGetMylynPriority() {
+		assertEquals("P1", MantisPriorityLevel.getMylynPriority("immediate").toString());
+		assertEquals("P2", MantisPriorityLevel.getMylynPriority("urgent").toString());
+		assertEquals("P2", MantisPriorityLevel.getMylynPriority("high").toString());
+		assertEquals("P3", MantisPriorityLevel.getMylynPriority("normal").toString());
+		assertEquals("P4", MantisPriorityLevel.getMylynPriority("low").toString());
+	}
+	
+	/**
+	 * Test to make sure the base url is being returned as expected.
+	 * @author David Carver
+	 */
+	public void testGetRepositoryBaseUrl() {
+		String baseUrl = "http://mylyn-mantis.sourceforge.net/Mantis/mc/mantisconnect.php";
+		
+		assertEquals("http://mylyn-mantis.sourceforge.net/Mantis/", MantisUtils.getRepositoryBaseUrl(baseUrl));
+	}
+}
