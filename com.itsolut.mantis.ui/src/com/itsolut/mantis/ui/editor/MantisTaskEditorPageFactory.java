26c2d0724df404501eacc92d86fc89bdb1f61b48
Robert Munteanu <rombert@sourceforge.net>
2008-12-18 07:23:40 +0000

    Tagged 3.0.1.200812171848

---
+++ com.itsolut.mantis.ui/src/com/itsolut/mantis/ui/editor/MantisTaskEditorPageFactory.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 Mylar committers and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package com.itsolut.mantis.ui.editor;
+
+import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.internal.provisional.commons.ui.CommonImages;
+import org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin;
+import org.eclipse.mylyn.tasks.ui.ITasksUiConstants;
+import org.eclipse.mylyn.tasks.ui.TasksUiImages;
+import org.eclipse.mylyn.tasks.ui.TasksUiUtil;
+import org.eclipse.mylyn.tasks.ui.editors.AbstractTaskEditorPageFactory;
+import org.eclipse.mylyn.tasks.ui.editors.TaskEditor;
+import org.eclipse.mylyn.tasks.ui.editors.TaskEditorInput;
+import org.eclipse.mylyn.commons.core.StatusHandler;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.forms.editor.FormPage;
+
+
+import com.itsolut.mantis.core.MantisCorePlugin;
+import com.itsolut.mantis.core.MantisRepositoryConnector;
+
+/**
+ * @author Steffen Pingel
+ */
+public class MantisTaskEditorPageFactory extends AbstractTaskEditorPageFactory {
+	
+	@Override
+	public boolean canCreatePageFor(TaskEditorInput input) {
+		if (input.getTask().getConnectorKind().equals(MantisCorePlugin.REPOSITORY_KIND)
+				|| TasksUiUtil.isOutgoingNewTask(input.getTask(), MantisCorePlugin.REPOSITORY_KIND)) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public FormPage createPage(TaskEditor parentEditor) {
+		return new MantisTaskEditorPage(parentEditor);
+	}
+
+	@Override
+	public Image getPageImage() {
+		// TODO Auto-generated method stub
+		return CommonImages.getImage(TasksUiImages.REPOSITORY);
+	}
+
+	@Override
+	public String getPageText() {
+		return "Mantis";
+	}
+	
+	@Override
+	public int getPriority() {
+		return PRIORITY_TASK;
+	}
+	
+	@Override
+	public String[] getConflictingIds(TaskEditorInput input) {
+		if (!input.getTask().getConnectorKind().equals(MantisCorePlugin.REPOSITORY_KIND)) {
+			return new String[] { ITasksUiConstants.ID_PAGE_PLANNING };
+		}
+		return null;
+	}
+	
+}
