9cff2a9e03c57a264ebfbf557284460828e1179c
Robert Munteanu <rombert@sourceforge.net>
2009-06-17 15:47:00 +0000

    Work towards proper p2 publishing - seems to work for now

--- com.itsolut.mantis-site/generate-p2-medatadata.sh
+++ com.itsolut.mantis-site/generate-p2-medatadata.sh
@@ -1,16 +1,19 @@
 #!/bin/sh -x
-eclipse_bin=~/apps/eclipse-3.5/eclipse
-repo_location=.
-repo_name="Mylyn-Mantis connector"
 
-$eclipse_bin \
-	-nosplash \
-	-application org.eclipse.equinox.p2.metadata.generator.EclipseGenerator \
-	-updateSite $repo_location \
-	-site $repo_location/site.xml \
-	-metadataRepository $repo_location -metadataRepositoryName "$repo_name Update Site"\
-	-artifactRepository $repo_location -artifactRepositoryName "$repo_name Artifacts"\
-	-compress \
-	-reusePack200files \
-	-noDefaultIUs \
-	-vmargs -Xmx256m 
+repo_name="Mylyn Mantis Connector"
+repository=/home/robert/workspaces/mylyn-mantis/com.itsolut.mantis-site
+target_dir=repo
+
+java -jar ~/apps/eclipse-3.5/plugins/org.eclipse.equinox.launcher_1.0.200.v20090520.jar \
+	-application org.eclipse.equinox.p2.publisher.UpdateSitePublisher \
+	-artifactRepository file:$repository/$target_dir \
+	-metadataRepository file:$repository/$target_dir \
+	-artifactRepositoryName "$repo_name Artifacts" \
+	-metadataRepositoryName "$repo_name Update Site" \
+	-source . \
+	-configs gtk.linux.x86 \
+	-publishArtifacts \
+	-append \
+	-compress
+
+cp site.xml $target_dir/
