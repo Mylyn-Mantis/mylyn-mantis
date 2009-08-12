1c4266475587b06a31654087b36e1db1fe9b0133
Robert Munteanu <rombert@sourceforge.net>
2009-08-12 20:21:32 +0000

    Tweaks to generate-p2-metadata.sh script

--- com.itsolut.mantis-site/generate-p2-medatadata.sh
+++ com.itsolut.mantis-site/generate-p2-medatadata.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -x
 
 repo_name="Mylyn Mantis Connector"
-repository=/home/robert/workspaces/mylyn-mantis/com.itsolut.mantis-site
+repository=$(pwd)
 target_dir=repo
+eclipse_home=${eclipse_home:-~/apps/eclipse/}
 
-java -jar ~/apps/eclipse/plugins/org.eclipse.equinox.launcher_1.0.200.v20090520.jar \
+java -jar ${eclipse_home}/plugins/org.eclipse.equinox.launcher_1.0.200.v20090520.jar \
 	-application org.eclipse.equinox.p2.publisher.UpdateSitePublisher \
 	-artifactRepository file:$repository/$target_dir \
 	-metadataRepository file:$repository/$target_dir \
@@ -15,6 +16,10 @@ java -jar ~/apps/eclipse/plugins/org.eclipse.equinox.launcher_1.0.200.v20090520.
 	-publishArtifacts \
 	-append \
 	-compress
+
+if [ $? -ne 0 ]; then
+	exit 1;
+fi
 
 cp -f site.xml $target_dir/
 
