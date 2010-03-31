af822011ae1e5d2e9cb46cddb8fd5a73f584c564
Robert Munteanu <rombert@sourceforge.net>
2010-03-31 23:10:10 +0000

    0000102: Fix unit tests
    Added a basic test for connector.performQuery.
    
    Still to be done:
    
    - more tests for performQuery ( new task found, task updated )
    - better repository url override hooks;
    - clean up task data handler test;
    - set up integration test on CI server;

--- com.itsolut.mantis.tests/src/com/itsolut/mantis/tests/AbstractMantisRepositoryConnectorIntegrationTest.java
+++ com.itsolut.mantis.tests/src/com/itsolut/mantis/tests/AbstractMantisRepositoryConnectorIntegrationTest.java
@@ -12,19 +12,33 @@ package com.itsolut.mantis.tests;
 import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.rpc.ServiceException;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.mylyn.internal.tasks.core.RepositoryQuery;
+import org.eclipse.mylyn.internal.tasks.core.sync.SynchronizationSession;
+import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
 import org.eclipse.mylyn.tasks.core.TaskRepositoryLocationFactory;
 import org.eclipse.mylyn.tasks.core.data.TaskAttribute;
 import org.eclipse.mylyn.tasks.core.data.TaskData;
+import org.eclipse.mylyn.tasks.core.data.TaskDataCollector;
+import org.eclipse.mylyn.tasks.core.sync.ISynchronizationSession;
 
 import com.itsolut.mantis.binding.IssueData;
 import com.itsolut.mantis.binding.ObjectRef;
+import com.itsolut.mantis.core.IMantisClient;
+import com.itsolut.mantis.core.MantisCache;
 import com.itsolut.mantis.core.MantisClientFactory;
 import com.itsolut.mantis.core.MantisRepositoryConnector;
+import com.itsolut.mantis.core.exception.MantisException;
+import com.itsolut.mantis.core.model.MantisProject;
+import com.itsolut.mantis.core.model.MantisProjectFilter;
 
 import junit.framework.TestCase;
 
@@ -65,7 +79,51 @@ public abstract class AbstractMantisRepositoryConnectorIntegrationTest extends T
 		assertAttributeEquals(taskData.getRoot().getMappedAttribute(TaskAttribute.SUMMARY), "First task");
 		assertAttributeEquals(taskData.getRoot().getMappedAttribute(TaskAttribute.DESCRIPTION), "Description");
 		assertAttributeEquals(taskData.getRoot().getMappedAttribute(TaskAttribute.STATUS), "new");
+
+	}
+
+	public void testPerformQuery() throws MantisException, MalformedURLException, RemoteException, ServiceException {
 		
+		createTask("First task", "Description");
+		createTask("Second task", "Description");
+		
+		MantisCache cache = repositoryAccessor.getClient().getCache(new NullProgressMonitor());
+		MantisProject project = cache.getProjectById(1);
+		
+		List<MantisProjectFilter> projectFilters = cache.getProjectFilters(1);
+		
+		assertEquals(projectFilters.toString(), 1, projectFilters.size());
+		
+		MantisProjectFilter filter = projectFilters.get(0);
+
+		final List<TaskData> hits = new ArrayList<TaskData>();
+
+		MantisRepositoryConnector connector = new MantisRepositoryConnector();
+
+		IRepositoryQuery query = new RepositoryQuery(connector.getConnectorKind(), "test");
+		query.setAttribute(IMantisClient.PROJECT_NAME, project.getName());
+		query.setAttribute(IMantisClient.FILTER_NAME, filter.getName());
+		TaskDataCollector resultCollector = new TaskDataCollector() {
+
+			@Override
+			public void accept(TaskData taskData) {
+				
+				hits.add(taskData);
+			}
+		};
+		ISynchronizationSession event = new SynchronizationSession();
+		IStatus status = connector.performQuery(repositoryAccessor.getRepository(), query, resultCollector, event,
+				new NullProgressMonitor());
+		
+		assertEquals("status", Status.OK_STATUS, status);
+		assertEquals("hits.size", 2, hits.size());
+		for ( TaskData taskData : hits ) {
+			assertNotNull("summary", taskData.getRoot().getMappedAttribute(TaskAttribute.SUMMARY));
+			assertNotNull("description", taskData.getRoot().getMappedAttribute(TaskAttribute.DESCRIPTION));
+			assertNotNull("status", taskData.getRoot().getMappedAttribute(TaskAttribute.STATUS));
+		}
+			
+
 	}
 
 	private void assertAttributeEquals(TaskAttribute mappedAttribute, String expectedValue) {
@@ -93,7 +151,7 @@ public abstract class AbstractMantisRepositoryConnectorIntegrationTest extends T
 	}
 
 	protected void setUp() throws Exception {
-		
+
 		MantisClientFactory.getDefault().setTaskRepositoryLocationFactory(new TaskRepositoryLocationFactory());
 
 		repositoryAccessor = new MantisRepositoryAccessor(getUsername(), getPassword(), getRepositoryUrlWithOverride());
