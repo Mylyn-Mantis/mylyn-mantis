3bea7dc2aade5740fc51407f10e34a15a46e7d94
Robert Munteanu <robert@lmn.ro>
2011-02-26 22:42:09 +0200

    Display real names in comments
    Affects #33: Display user names and emails in the People part

---
+++ com.itsolut.mantis.core/src/com/itsolut/mantis/core/User.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (C) 2010 Robert Munteanu <robert.munteanu@gmail.com>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package com.itsolut.mantis.core;
+
+import java.io.Serializable;
+
+/**
+ * @author Robert Munteanu
+ */
+public class User implements Serializable {
+
+    private String username;
+    private String realName;
+    private String email;
+
+    public User(String username, String realName, String email) {
+
+        this.username = username;
+        this.realName = realName;
+        this.email = email;
+    }
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public String getRealName() {
+
+        return realName;
+    }
+
+}
