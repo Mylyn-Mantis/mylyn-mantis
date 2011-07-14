381d6bddfc64932e2be50a0960a1c6e05c63367f
Robert Munteanu <robert@lmn.ro>
2011-07-14 19:10:08 +0300

    Start moving tests to proper packages

--- com.itsolut.mantis.tests/src/com/itsolut/mantis/core/util/HtmlFormatterTest.java
+++ com.itsolut.mantis.tests/src/com/itsolut/mantis/core/util/HtmlFormatterTest.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package com.itsolut.mantis.tests;
+package com.itsolut.mantis.core.util;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
