cc312eb4ce4970b1ddb80dae2b54e6dd376e87b2
Robert Munteanu <robert@lmn.ro>
2011-07-15 13:46:39 +0300

    MantisConverter: add tests for converting projects

--- com.itsolut.mantis.tests/src/com/itsolut/mantis/core/soap/MantisConverterTest.java
+++ com.itsolut.mantis.tests/src/com/itsolut/mantis/core/soap/MantisConverterTest.java
@@ -17,9 +17,13 @@ import java.util.List;
 import org.junit.Test;
 
 import biz.futureware.mantis.rpc.soap.client.ObjectRef;
+import biz.futureware.mantis.rpc.soap.client.ProjectData;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.itsolut.mantis.core.exception.MantisException;
 import com.itsolut.mantis.core.model.MantisETA;
+import com.itsolut.mantis.core.model.MantisProject;
 
 /**
  * @author Robert Munteanu
@@ -54,5 +58,24 @@ public class MantisConverterTest {
 		
 		assertEquals(1, first.getValue());
 		assertEquals("One", first.getName());
+	}
+	
+	@Test
+	public void convertProjects() {
+		
+		final ProjectData child = new ProjectData(BigInteger.TEN, "First", null, null, null, null, null, null, null, null);
+		ProjectData first = new ProjectData(BigInteger.ONE, "First", null, null, null, null, null, null, new ProjectData[] { child }, null);
+		ProjectData second = new ProjectData(BigInteger.valueOf(2), "First", null, null, null, null, null, null, null, null);
+		
+		List<MantisProject> projects = MantisConverter.convert(new ProjectData[] { first, second } );
+		
+		assertEquals(3, projects.size());
+		MantisProject project = Iterables.find(projects, new Predicate<MantisProject>() {
+			public boolean apply(MantisProject input) {
+				return input.getValue() == child.getId().intValue();
+			}
+		});
+
+		assertEquals(Integer.valueOf(1), project.getParentProjectId());
 	}
 }
